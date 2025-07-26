2c64541f68d03e4a98a94f7e4e7c8ca60c4ebd11
IMhongkeun <hongeun15@gmail.com>
2025-07-26 16:42:56 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,15 @@ def search_pubmed():
     query = request.args.get("query")
     # 논문 검색 API 연동 코드는 나중에 추가
     return jsonify({"articles": [
-        {"title": "Sample Title", "authors": "Smith J", "journal": "NEJM", "year": "2024", "pmid": "123456", "abstract": "Sample abstract."}
+        {
+            "title": "Sample Title",
+            "authors": "Smith J",
+            "journal": "NEJM",
+            "year": "2024",
+            "pmid": "123456",
+            "url": "https://pubmed.ncbi.nlm.nih.gov/123456/",
+            "abstract": "Sample abstract."
+        }
     ]})
 
 if __name__ == "__main__":
