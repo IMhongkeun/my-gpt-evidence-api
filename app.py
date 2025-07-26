f656874d076b74e25edce2dbc4bcf4a6a12bcbbc
IMhongkeun <hongeun15@gmail.com>
2025-07-26 16:15:18 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/pubmed/search")
+def search_pubmed():
+    query = request.args.get("query")
+    # 논문 검색 API 연동 코드는 나중에 추가
+    return jsonify({"articles": [
+        {"title": "Sample Title", "authors": "Smith J", "journal": "NEJM", "year": "2024", "pmid": "123456", "abstract": "Sample abstract."}
+    ]})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
